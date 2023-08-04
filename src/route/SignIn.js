7997173c930120cafef99a21f3d5696cbd335328
HaraYoung <qkrtpdud9899@gmail.com>
2023-08-05 01:42:57 +0900

    로그인 기능 구현, 로그인 여부에 따른 리다이렉트 처리 구현 과제 완료

--- src/route/SignIn.js
+++ src/route/SignIn.js
@@ -1,27 +1,63 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useNavigate, useMatch } from "react-router-dom";
 
 const SignIn = () => {
+  const navigate = useNavigate();
+  const signinMatch = useMatch("signin");
+  useEffect(() => {
+    if (localStorage.getItem("access_token")) {
+      if (signinMatch) navigate("/todo");
+    }
+  }, []);
+  const API = "https://www.pre-onboarding-selection-task.shop";
+  let emailInputValue = "";
+  let passwordInputValue = "";
+  const onChangeEmail = (e) => (emailInputValue = e.target.value);
+  const onChangePassword = (e) => (passwordInputValue = e.target.value);
+  const postSignin = async (e) => {
+    e.preventDefault();
+    //email에 '@'포함될 경우 true 할당
+    let hasAtSymbol = emailInputValue.includes("@");
+    //email : '@' 포함되지 않은 경우, password: 8자 미만일 경우
+    if (!hasAtSymbol || passwordInputValue.length < 8) {
+      document.getElementById("signin-button").disabled = true;
+    } else {
+      try {
+        const response = await axios.post(`${API}/auth/signin`, {
+          email: emailInputValue,
+          password: passwordInputValue,
+        });
+        console.log(response);
+        const { access_token } = response.data;
+        localStorage.setItem("access_token", access_token);
+        navigate("/todo");
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
   return (
     <div>
       <div>SignIn</div>
-      <form>
-        <input data-testid="email-input" />
-        <input data-testid="password-input" minLength={8} />
-        <button data-testid="signin-button">로그인</button>
+      <form onSubmit={postSignin}>
+        <input data-testid="email-input" onChange={onChangeEmail} />
+        <input
+          data-testid="password-input"
+          minLength={8}
+          type="password"
+          onChange={onChangePassword}
+        />
+        <button
+          id="signin-button"
+          data-testid="signin-button"
+          onClick={postSignin}
+        >
+          로그인
+        </button>
       </form>
     </div>
   );
 };
 
 export default SignIn;
-
-// 회원가입과 로그인 페이지에 이메일과 비밀번호의 유효성 검사기능을 구현
-
-// 이메일 조건: @ 포함
-// 비밀번호 조건: 8자 이상
-// 이메일과 비밀번호의 유효성 검사 조건은 별도의 추가 조건 부여 없이 위의 조건대로만 진행
-// (e.g. 비밀번호 유효성 검사에 특수문자 포함 등의 새로운 조건을 추가하는 행위,
-// 비밀번호 확인 조건을 추가하는 행위 등은 지양해주세요)
-// 입력된 이메일과 비밀번호가 유효성 검사를 통과하지 못한다면 button에 disabled 속성을 부여
-
-// 보안 상 실제 사용하고 계신 이메일과 패스워드말고 테스트용 이메일, 패스워드 사용을 권장.
--- src/route/Signup.js
+++ src/route/Signup.js
@@ -1,34 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useMatch } from "react-router-dom";
 
 const Signup = () => {
-  const API = "https://www.pre-onboarding-selection-task.shop/";
   const navigate = useNavigate();
+  const signinMatch = useMatch("signup");
+  useEffect(() => {
+    if (localStorage.getItem("access_token")) {
+      if (signinMatch) navigate("/todo");
+    }
+  }, []);
+  const API = "https://www.pre-onboarding-selection-task.shop";
   let emailInputValue = "";
   let passwordInputValue = "";
   const onChangeEmail = (e) => (emailInputValue = e.target.value);
   const onChangePassword = (e) => (passwordInputValue = e.target.value);
   const postSignup = (e) => {
     e.preventDefault();
-    //email에 '@'포함될 경우 [@]반환, 그렇지 않다면 []반환
-    let hasAtSymbol = emailInputValue.split("").filter((item) => item === "@");
+    //email에 '@'포함될 경우 true 반환
+    let hasAtSymbol = emailInputValue.includes("@");
     //email : '@' 포함되지 않은 경우, password: 8자 미만일 경우
-    if (hasAtSymbol.length < 1 || passwordInputValue.length < 8) {
-      alert("hasAtSymbol false");
+    if (!hasAtSymbol || passwordInputValue.length < 8) {
       document.getElementById("signup-button").disabled = true;
     } else {
       axios
-        .post(`${API}auth/signup`, {
+        .post(`${API}/auth/signup`, {
           email: emailInputValue,
           password: passwordInputValue,
         })
         .then(function (response) {
           console.log(response);
-          navigate('/signin')
+          navigate("/signin");
         })
         .catch(function (error) {
           console.log(error);
+          alert(error.response.data.message);
         });
     }
   };
