35d85e9ef1e851c11ba2bb865084550ae50096b9
HaraYoung <qkrtpdud9899@gmail.com>
2023-08-06 23:55:30 +0900

    todo 수정,삭제 기능 구현

--- src/route/Todo.js
+++ src/route/Todo.js
@@ -6,7 +6,26 @@ const Todo = () => {
   const accessToken = localStorage.getItem("access_token");
   const [todos, setTodos] = useState(null);
   let newTodo = "";
+  const [isEdit, setIsEdit] = useState([]);
   const onChangeNewTodo = (e) => (newTodo = e.target.value);
+
+  const getTodos = async () => {
+    try {
+      const response = await axios.get(`${API}/todos`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      console.log(response);
+      setTodos(response.data);
+      setIsEdit(new Array(response.data.length).fill(false));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getTodos();
+  }, [newTodo]);
   const postTodo = async (e) => {
     e.preventDefault();
     try {
@@ -27,29 +46,18 @@ const Todo = () => {
       console.log(error);
     }
   };
-  const getTodos = async () => {
-    try {
-      const response = await axios.get(`${API}/todos`, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
-      console.log(response);
-      setTodos(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  useEffect(() => {
-    getTodos();
-  }, [newTodo]);
 
+  const onClickEdit = (index) => {
+    const newIsEdit = [...isEdit];
+    newIsEdit[index] = !newIsEdit[index];
+    setIsEdit(newIsEdit);
+  };
   const putTodosIsCompleted = async (isCheck, id, todo) => {
     try {
       const response = await axios.put(
         `${API}/todos/${id}`,
         {
-          todo:todo,
+          todo: todo,
           isCompleted: isCheck,
         },
         {
@@ -59,14 +67,29 @@ const Todo = () => {
         }
       );
       console.log(response);
+      getTodos();
     } catch (error) {
       console.log(error);
     }
   };
-  const isCompletedCheckChange = (isCheck, id, todo) => {
+  let isCompleted = false;
+  let editInputValue = "";
+  const onChangeTodoEdit = (isCheck, id, todo) => {
     putTodosIsCompleted(isCheck, id, todo);
   };
 
+  const deleteTodo = async (id) => {
+    try {
+      await axios.delete(`${API}/todos/${id}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      getTodos();
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
       <div>Todo</div>
@@ -76,19 +99,57 @@ const Todo = () => {
       </form>
       <ul>
         {todos &&
-          todos.map((item) => (
+          todos.map((item, index) => (
             <li key={item.id}>
               <label>
                 <input
                   type="checkbox"
-                  onChange={(e) =>
-                    isCompletedCheckChange(e.target.checked, item.id, item.todo)
-                  }
+                  checked={item.isCompleted}
+                  onChange={(e) => {
+                    onChangeTodoEdit(e.target.checked, item.id, item.todo);
+                    isCompleted = e.target.checked;
+                  }}
                 />
                 <span>{item.todo}</span>
               </label>
-              <button data-testid="modify-button">수정</button>
-              <button data-testid="delete-button">삭제</button>
+              {isEdit[index] ? (
+                <span>
+                  <input
+                    data-testid="modify-input"
+                    defaultValue={item.todo}
+                    onChange={(e) => (editInputValue = e.target.value)}
+                  />
+                  <button
+                    data-testid="submit-button"
+                    onClick={(e) =>
+                      onChangeTodoEdit(isCompleted, item.id, editInputValue)
+                    }
+                  >
+                    제출
+                  </button>
+                  <button
+                    data-testid="cancel-button"
+                    onClick={() => onClickEdit(index)}
+                  >
+                    취소
+                  </button>
+                </span>
+              ) : (
+                <span>
+                  <button
+                    data-testid="modify-button"
+                    onClick={() => onClickEdit(index)}
+                  >
+                    수정
+                  </button>
+                  <button
+                    data-testid="delete-button"
+                    onClick={() => deleteTodo(item.id)}
+                  >
+                    삭제
+                  </button>
+                </span>
+              )}
             </li>
           ))}
       </ul>
